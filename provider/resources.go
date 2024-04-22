3662e5be7c6c06f8e8c0911de4d35935f89255ec
Ian Wahbe <ian@wahbe.com>
2024-04-22 13:24:53 -0700

    Remove unneeded resource mappings

--- provider/resources.go
+++ provider/resources.go
@@ -96,47 +96,20 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		Resources: map[string]*tfbridge.ResourceInfo{
 			"databricks_default_namespace_setting": {
-				Docs: &tfbridge.DocInfo{
-					Source: "default_namespace_settings.md",
-				},
+				Docs: &tfbridge.DocInfo{Source: "default_namespace_settings.md"},
 			},
 
-			"databricks_catalog":            {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Catalog")},
-			"databricks_cluster":            {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Cluster")},
-			"databricks_cluster_policy":     {Tok: tfbridge.MakeResource(mainPkg, mainMod, "ClusterPolicy")},
-			"databricks_dbfs_file":          {Tok: tfbridge.MakeResource(mainPkg, mainMod, "DbfsFile")},
-			"databricks_directory":          {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Directory")},
-			"databricks_entitlements":       {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Entitlements")},
-			"databricks_external_location":  {Tok: tfbridge.MakeResource(mainPkg, mainMod, "ExternalLocation")},
-			"databricks_git_credential":     {Tok: tfbridge.MakeResource(mainPkg, mainMod, "GitCredential")},
-			"databricks_global_init_script": {Tok: tfbridge.MakeResource(mainPkg, mainMod, "GlobalInitScript")},
 			"databricks_grants": {
-				Tok: tfbridge.MakeResource(mainPkg, mainMod, "Grants"),
 				Fields: map[string]*tfbridge.SchemaInfo{
-					"grant": {
-						CSharpName: "GrantDetails",
-					},
+					"grant": {CSharpName: "GrantDetails"},
 				},
 			},
 			"databricks_group": {
-				Tok: tfbridge.MakeResource(mainPkg, mainMod, "Group"),
 				Fields: map[string]*tfbridge.SchemaInfo{
 					"display_name": tfbridge.AutoName("displayName", 255, "-"),
 				},
 			},
-			"databricks_group_instance_profile": {Tok: tfbridge.MakeResource(mainPkg, mainMod, "GroupInstanceProfile")},
-			"databricks_group_member": {
-				Tok: tfbridge.MakeResource(mainPkg, mainMod, "GroupMember"),
-				Docs: &tfbridge.DocInfo{
-					Markdown: docGroupMember,
-				},
-			},
-			"databricks_group_role":       {Tok: tfbridge.MakeResource(mainPkg, mainMod, "GroupRole")},
-			"databricks_instance_pool":    {Tok: tfbridge.MakeResource(mainPkg, mainMod, "InstancePool")},
-			"databricks_instance_profile": {Tok: tfbridge.MakeResource(mainPkg, mainMod, "InstanceProfile")},
-			"databricks_ip_access_list":   {Tok: tfbridge.MakeResource(mainPkg, mainMod, "IpAccessList")},
-			"databricks_job":              {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Job")},
-			"databricks_library":          {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Library")},
+			"databricks_group_member": {Docs: &tfbridge.DocInfo{Markdown: docGroupMember}},
 
 			// The documentation for the `datastore_provider` resource describes:
 			//
@@ -144,80 +117,22 @@ func Provider() tfbridge.ProviderInfo {
 			// which contains a list of shares that have been shared with you.
 			//
 			// `databricks:index:Provider` is an illegal token, so `databricks_provider` is being mapped to `MetastoreProvider`.
-			"databricks_provider":              {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MetastoreProvider")},
-			"databricks_metastore":             {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Metastore")},
-			"databricks_metastore_assignment":  {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MetastoreAssignment")},
-			"databricks_metastore_data_access": {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MetastoreDataAccess")},
+			"databricks_provider": {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MetastoreProvider")},
 
-			"databricks_mlflow_experiment":           {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MlflowExperiment")},
-			"databricks_mlflow_model":                {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MlflowModel")},
-			"databricks_mlflow_webhook":              {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MlflowWebhook")},
-			"databricks_mount":                       {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Mount")},
-			"databricks_mws_credentials":             {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MwsCredentials")},
-			"databricks_mws_customer_managed_keys":   {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MwsCustomerManagedKeys")},
-			"databricks_mws_log_delivery":            {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MwsLogDelivery")},
-			"databricks_mws_networks":                {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MwsNetworks")},
-			"databricks_mws_private_access_settings": {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MwsPrivateAccessSettings")},
-			"databricks_mws_storage_configurations":  {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MwsStorageConfigurations")},
-			"databricks_mws_vpc_endpoint":            {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MwsVpcEndpoint")},
-			"databricks_mws_workspaces":              {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MwsWorkspaces")},
-			"databricks_mws_permission_assignment":   {Tok: tfbridge.MakeResource(mainPkg, mainMod, "MwsPermissionAssignment")},
-			"databricks_notebook":                    {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Notebook")},
-			"databricks_obo_token":                   {Tok: tfbridge.MakeResource(mainPkg, mainMod, "OboToken")},
-			"databricks_permissions":                 {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Permissions")},
 			"databricks_permission_assignment": {
-				Tok: tfbridge.MakeResource(mainPkg, mainMod, "PermissionAssignment"),
-				Docs: &tfbridge.DocInfo{
-					Markdown: []byte(" "),
-				},
+				Docs: &tfbridge.DocInfo{Markdown: []byte(" ")},
 			},
-			"databricks_pipeline":               {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Pipeline")},
-			"databricks_recipient":              {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Recipient")},
-			"databricks_repo":                   {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Repo")},
-			"databricks_schema":                 {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Schema")},
-			"databricks_secret":                 {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Secret")},
-			"databricks_secret_acl":             {Tok: tfbridge.MakeResource(mainPkg, mainMod, "SecretAcl")},
-			"databricks_secret_scope":           {Tok: tfbridge.MakeResource(mainPkg, mainMod, "SecretScope")},
-			"databricks_service_principal":      {Tok: tfbridge.MakeResource(mainPkg, mainMod, "ServicePrincipal")},
-			"databricks_service_principal_role": {Tok: tfbridge.MakeResource(mainPkg, mainMod, "ServicePrincipalRole")},
 			"databricks_service_principal_secret": {
-				Tok:  tfbridge.MakeResource(mainPkg, mainMod, "ServicePrincipalSecret"),
 				Docs: &tfbridge.DocInfo{Markdown: []byte{' '}},
 			},
 			"databricks_table": {
-				Tok:  tfbridge.MakeResource(mainPkg, mainMod, "Table"),
 				Docs: &tfbridge.DocInfo{Markdown: []byte{' '}},
 			},
 		},
 		DataSources: map[string]*tfbridge.DataSourceInfo{
-			"databricks_aws_assume_role_policy":  {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getAwsAssumeRolePolicy")},
-			"databricks_aws_bucket_policy":       {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getAwsBucketPolicy")},
-			"databricks_aws_crossaccount_policy": {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getAwsCrossAccountPolicy")},
-			"databricks_catalogs":                {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCatalogs")},
-			"databricks_clusters":                {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getClusters")},
-			"databricks_current_user":            {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCurrentUser")},
-			"databricks_dbfs_file":               {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getDbfsFile")},
-			"databricks_dbfs_file_paths":         {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getDbfsFilePaths")},
-			"databricks_group":                   {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getGroup")},
-			"databricks_job":                     {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getJob")},
-			"databricks_jobs":                    {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getJobs")},
-			"databricks_node_type":               {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getNodeType")},
-			"databricks_notebook":                {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getNotebook")},
-			"databricks_notebook_paths":          {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getNotebookPaths")},
-			"databricks_schemas":                 {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getSchemas")},
-			"databricks_spark_version":           {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getSparkVersion")},
-			"databricks_tables":                  {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getTables")},
-			"databricks_user":                    {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getUser")},
-			"databricks_views":                   {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getViews")},
-			"databricks_zones":                   {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getZones")},
-			"databricks_service_principal":       {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getServicePrincipal")},
-			"databricks_service_principals":      {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getServicePrincipals")},
-			"databricks_cluster":                 {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getCluster")},
-			"databricks_share":                   {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getShare")},
-			"databricks_shares":                  {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getShares")},
-			"databricks_sql_warehouse":           {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getSqlWarehouse")},
-			"databricks_sql_warehouses":          {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getSqlWarehouses")},
-			"databricks_mws_workspaces":          {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getMwsWorkspaces")},
+			"databricks_aws_crossaccount_policy": {
+				Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getAwsCrossAccountPolicy"),
+			},
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
@@ -228,15 +143,12 @@ func Provider() tfbridge.ProviderInfo {
 				"@types/mime": "^2.0.0",
 			},
 		},
-		Python: (func() *tfbridge.PythonInfo {
-			i := &tfbridge.PythonInfo{
-				Requires: map[string]string{
-					"pulumi": ">=3.0.0,<4.0.0",
-				}}
-			i.PyProject.Enabled = true
-			return i
-		})(),
-
+		Python: &tfbridge.PythonInfo{
+			Requires: map[string]string{
+				"pulumi": ">=3.0.0,<4.0.0",
+			},
+			PyProject: struct{ Enabled bool }{true},
+		},
 		Golang: &tfbridge.GolangInfo{
 			ImportBasePath: filepath.Join(
 				fmt.Sprintf("github.com/pulumi/pulumi-%[1]s/sdk/", mainPkg),
